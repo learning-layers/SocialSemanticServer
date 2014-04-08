cfa2d0d1359ee6c4beaedf649005e9e2a7b1852b
Dieter Theiler <dtheiler@tugraz.at>
2014-04-08 15:20:58 +0200

    license headers adaption

--- ss/src/main/java/at/kc/tugraz/ss/test/accessrights/SSAccessRightsTester.java
+++ ss/src/main/java/at/kc/tugraz/ss/test/accessrights/SSAccessRightsTester.java
@@ -1,5 +1,10 @@
 /**
- * Copyright 2014 Graz University of Technology - KTI (Knowledge Technologies Institute)
+ * Code contributed to the Learning Layers project
+ * http://www.learning-layers.eu
+ * Development is partly funded by the FP7 Programme of the European Commission under
+ * Grant Agreement FP7-ICT-318209.
+ * Copyright (c) 2014, Graz University of Technology - KTI (Knowledge Technologies Institute).
+ * For a list of contributors see the AUTHORS file at the top-level directory of this distribution.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
