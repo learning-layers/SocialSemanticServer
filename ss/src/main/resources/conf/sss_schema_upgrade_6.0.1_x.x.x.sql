f9d1a5d8368a43ab475bdebe501e1869cb5bd2a0
dtheiler <dtheiler@tugraz.at>
2014-11-07 12:38:42 +0100

    LL-803

--- ss/src/main/resources/conf/sss_schema_upgrade_6.0.1_x.x.x.sql
+++ ss/src/main/resources/conf/sss_schema_upgrade_6.0.1_x.x.x.sql
@@ -1,6 +1,18 @@
 ALTER TABLE `sss`.`categoryass` 
 ADD COLUMN `creationTime` VARCHAR(200) NOT NULL AFTER `categorySpace`;
 
+CREATE TABLE `sss`.`user` (
+  `userId` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`userId`));
+
+ALTER TABLE `sss`.`user` 
+ADD CONSTRAINT `userIdFKuser`
+  FOREIGN KEY (`userId`)
+  REFERENCES `sss`.`entity` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+  
 CREATE TABLE `sss`.`message` (
   `messageId` VARCHAR(200) NOT NULL,
   `userId` VARCHAR(200) NOT NULL,
