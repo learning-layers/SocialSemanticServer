6108769a40194723d0842df9f424484344188bbb
Dieter Theiler <dtheiler@tugraz.at>
2015-04-20 10:59:32 +0200

    SSS-155 adapting sss docker stuff

--- sss/src/main/resources/conf/docker.sss/sss.sss.docker.entrypoint.sh
+++ sss/src/main/resources/conf/docker.sss/sss.sss.docker.entrypoint.sh
@@ -4,6 +4,22 @@ trap 'exit' ERR
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
 cd /sss.app/
+
+sed -i "s#SSS_MYSQL_HOST#${SSS_MYSQL_HOST}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_MYSQL_PORT#${SSS_MYSQL_PORT}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_MYSQL_USERNAME#${SSS_MYSQL_USERNAME}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_MYSQL_PASSWORD#${SSS_MYSQL_PASSWORD}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_MYSQL_SCHEME#${SSS_MYSQL_SCHEME}#g" /sss.app/sss.conf.yaml &&
+
+sed -i "s#SSS_AUTH_TYPE#${SSS_AUTH_TYPE}#g" /sss.app/sss.conf.yaml &&
+
+sed -i "s#SSS_TETHYS_USER#${SSS_TETHYS_USER}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_TETHYS_PASSWORD#${SSS_TETHYS_PASSWORD}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_TETHYS_LAS_USER#${SSS_TETHYS_LAS_USER}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_TETHYS_LAS_PASSWORD#${SSS_TETHYS_LAS_PASSWORD}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_TETHYS_OIDC_CONF_URI#${SSS_TETHYS_OIDC_CONF_URI}#g" /sss.app/sss.conf.yaml &&
+sed -i "s#SSS_TETHYS_OIDC_USER_END_POINT_URI#${SSS_TETHYS_OIDC_USER_END_POINT_URI}#g" /sss.app/sss.conf.yaml &&
+
 java -jar -Dlog4j.configuration=file:log4j.properties sss.jar
 
 read
--- sss/src/main/resources/conf/docker.sss/sss.sss.run.container.sh
+++ sss/src/main/resources/conf/docker.sss/sss.sss.run.container.sh
@@ -1,4 +1,20 @@
 echo run sss container
 docker stop sss.sss
 docker rm sss.sss
-docker run -p 8391:8390 --name sss.sss ssss/sss.sss
+docker run \
+-d \
+-e "SSS_MYSQL_HOST=db_host" \
+-e "SSS_MYSQL_PORT=3333" \
+-e "SSS_MYSQL_USERNAME=sss" \
+-e "SSS_MYSQL_PASSWORD=sss" \
+-e "SSS_MYSQL_SCHEME=sss" \
+-e "SSS_AUTH_TYPE=csvFileAuth" \
+-e "SSS_TETHYS_USER=tethys_user" \
+-e "SSS_TETHYS_PASSSWORD=tethys_password" \
+-e "SSS_TETHYS_LAS_USER=sss" \
+-e "SSS_TETHYS_LAS_PASSWORD=sss" \
+-e "SSS_TETHYS_OIDC_CONF_URI=https://api.learning-layers.eu/o/oauth2/.well-known/openid-configuration" \
+-e "SSS_TETHYS_OIDC_USER_END_POINT_URI=https://api.learning-layers.eu/o/oauth2/userinfo" \
+-p 8391:8390 \
+--name sss.sss \
+ssss/sss.sss
